30bbd4bbe85aac9daabbd8bf4ddcd2d9d5ecd1ed
Christopher <chrisf7804@gmail.com>
2026-09-17 01:11:54 -0700

    Created test for incrementing a counter and to prevent updating a non-existent counter also implemented code for green phase

--- tdd_lab/src/counter.py
+++ tdd_lab/src/counter.py
@@ -19,3 +19,11 @@ def create_counter(name):
         return jsonify({"error": f"Counter {name} already exists"}), status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return jsonify({name: COUNTERS[name]}), status.HTTP_201_CREATED
+
+@app.route('/counters/<name>', methods =["PUT"])
+def increment_counter(name):
+    """increment a counter"""
+    if not counter_exists(name):
+        return jsonify({"error": f"Counter {name} does not exist"}), status.HTTP_404_NOT_FOUND
+    COUNTERS[name] += 1
+    return jsonify({name: COUNTERS[name]}), status.HTTP_200_OK
