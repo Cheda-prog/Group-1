0e109e82bf33cb2aa14985f960d5ced608d47343
Cheda-prog <chedaalex6@gmail.com>
2026-09-18 20:34:46 -0700

    Demonstrate split-jobs failure isolation: introduce a failing test

--- ci_lab/tests/test_counter.py
+++ ci_lab/tests/test_counter.py
@@ -41,7 +41,8 @@ class TestCounterEndpoints:
         """It should create a counter"""
         response = client.post('/counters/test_counter')
         assert response.status_code == status.HTTP_201_CREATED
-        assert response.get_json() == {"test_counter": 0}
+        # TEMPORARY CONTROLLED FAILURE: intentionally wrong expected value
+        assert response.get_json() == {"test_counter": 999}
 
     def test_prevent_duplicate_counter(self, client):
         """It should not allow duplicate counters"""
